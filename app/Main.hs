217321a3e4dbb0ddd661d2863731929e9b766a1f
Dmitry Olshansky <olshanskydr@gmail.com>
2019-02-09 13:31:13 +0300

    Upd: app

--- app/Main.hs
+++ app/Main.hs
@@ -13,9 +13,16 @@ data T1 = T1
   , t13 :: Maybe (Tagged (TLt 10) Text) }
   deriving (Show, Eq)
 
+data T2 = T2
+  { t21 :: T1
+  , t22 :: Tagged (TEnum '["foo","bar","baz"]) Text }
+  deriving (Show, Eq)
+
 genRecInfo ''T1
+genRecInfo ''T2
 
 instance Validatable T1
+instance Validatable T2
 
 main :: IO ()
 main = do
@@ -25,3 +32,9 @@ main = do
   print $ not $ validate $ T1 123 (Tagged 15) (Just $ Tagged "qwerqwer14234")
   print $ not $ validate $ T1 123 (Tagged 150) (Just $ Tagged "qwerqwer")
   print $ not $ validate $ T1 123 (Tagged 5) (Just $ Tagged "qwerqwer")
+  print $ validate
+    $ T2 (T1 123 (Tagged 15) (Just $ Tagged "qwerqwer")) (Tagged "bar")
+  print $ not $ validate
+    $ T2 (T1 123 (Tagged 15) (Just $ Tagged "qwerqwer")) (Tagged "boom")
+  print $ not $ validate
+    $ T2 (T1 123 (Tagged 150) (Just $ Tagged "qwerqwer")) (Tagged "foo")
